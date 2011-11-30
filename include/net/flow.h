d32ed8fa29290ffcda90c9468e691b6853a1a410
Eric Dumazet <eric.dumazet@gmail.com>
2011-11-30 19:00:53 +0000

    ipv4: use a 64bit load/store in output path
    gcc compiler is smart enough to use a single load/store if we
    memcpy(dptr, sptr, 8) on x86_64, regardless of
    CONFIG_CC_OPTIMIZE_FOR_SIZE
    
    In IP header, daddr immediately follows saddr, this wont change in the
    future. We only need to make sure our flowi4 (saddr,daddr) fields wont
    break the rule.
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Change-Id: Iad9c8fd9121ec84c2599b013badaebba92db7c39
    
    Conflicts:
    	include/net/flow.h

--- include/net/flow.h
+++ include/net/flow.h
@@ -60,9 +60,12 @@ struct flowi4 {
 #define flowi4_proto		__fl_common.flowic_proto
 #define flowi4_flags		__fl_common.flowic_flags
 #define flowi4_secid		__fl_common.flowic_secid
-#define flowi4_uid		__fl_common.flowic_uid
-	__be32			daddr;
+#define flowi4_secid		__fl_common.flowic_secid
+
+	/* (saddr,daddr) must be grouped, same order as in IP header */
 	__be32			saddr;
+	__be32			daddr;
+
 	union flowi_uli		uli;
 #define fl4_sport		uli.ports.sport
 #define fl4_dport		uli.ports.dport
